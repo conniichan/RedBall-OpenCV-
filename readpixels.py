3527ba5f5bec44cb25b56feabc3ec3eb9ecd543e
Connie Zeng <85717828+conniichan@users.noreply.github.com>
2025-06-26 00:06:18 -0700

    Converted BGR format to RGB format and Used matplotlib to display image

--- readpixels.py
+++ readpixels.py
@@ -6,6 +6,7 @@ def readAndWriteSinglePixel(): #this is defining a function
     root = os.getcwd() #get current working directory
     imgPath = os.path.join(root, 'images','redball.jpeg') #create path to image
     img = cv.imread(imgPath) #reads the image from the path 
+    RGBimg = cv.cvtColor(img, cv.COLOR_BGR2RGB) #converts BGR to RGB
 
     if img is None: #checks if the image was read correctly
         print('Image not found') 
@@ -14,12 +15,10 @@ def readAndWriteSinglePixel(): #this is defining a function
         print('Image found')
     
 
+    plt.imshow(RGBimg)
+    plt.waitforbuttonpress()
+    plt.close('all')
     
     
-    cv.imshow('redball', img) #displays the image in a window
-    cv.waitKey(0) #keeps the window open until a key is pressed
-    cv.destroyAllWindows() #emnsures window is closed after key pressed and 
-    #any resources used are released
-
 if  __name__ == "__main__":
     readAndWriteSinglePixel()
